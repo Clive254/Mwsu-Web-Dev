c62b5ff1aab0c19b6ce4a10c02f9cc02b765a1f7
clive254 <matikuclive@gmail.com>
2016-07-19 10:13:06 -0400

    Prog_2

---
+++ Program_2/home.php
@@ -0,0 +1,42 @@
+
+<?php
+
+//ob_start();
+//session_start();
+
+
+//include_once 'dbconnect.php';
+
+ //if(!isset($_SESSION['user_id']) ){
+	//header("Location: register.php");
+	//exit;
+
+print_r($_SESSION); 
+
+echo"<h1> Home Page </h1>";
+//$conn->close();
+?>
+<html>
+<head>
+<style>
+		/* Credit to bootsnipp.com for the css for the color graph */
+		.colorgraph {
+		  height: 5px;
+		  border-top: 0;
+		  background: #c4e17f;
+		  border-radius: 5px;
+		  background-image: -webkit-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+		  background-image: -moz-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+		  background-image: -o-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+		  background-image: linear-gradient(to right, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+		}
+	</style>
+<title>Home</title>
+<body>
+<li><a href="https://www.google.com/intl/en/mail/help/about.html">Gmail</a>
+<li><a href="https://login.yahoo.com/?.src=ym&.intl=us&.lang=en-US&.done=https%3a//mail.yahoo.com">YahOO!</a>
+<li><a href="https://login.live.com/login.srf?wa=wsignin1.0&ct=1468935750&rver=6.6.6556.0&wp=MBI_SSL&wreply=https:%2F%2Foutlook.live.com%2Fowa%2F&id=292841&CBCXT=out&bk=1468935751&mkt=EN-US&lc=1033&cred=otc">Outlook</a>
+
+</body>
+</head>
+</html>
